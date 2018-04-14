084fbeb97c5a6e5162cff584e22081d7bd9c5210
Annalise Ko <annaliseliuko@gmail.com>
2018-04-14 16:27:44 -0700

    fixed enum

--- sequence.sql
+++ sequence.sql
@@ -5,11 +5,12 @@ CREATE DATABASE sequence;
 USE sequence;
 
 CREATE TABLE Students (
-  s_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(256) NOT NULL,
-  password VARCHAR(256) NOT NULL,
-  college ENUM('CMC','HMC','Scripps') NOT NULL,
-  year INT(4) UNSIGNED NOT NULL,
+  s_id INT(8) UNSIGNED NOT NULL PRIMARY KEY,
+  firstname VARCHAR(256) NOT NULL,
+  lastname VARCHAR(256) NOT NULL,
+  pwd VARCHAR(256) NOT NULL,
+  college ENUM('Claremont McKenna College','Harvey Mudd College','Scripps College') NOT NULL,
+  grad INT(4) UNSIGNED NOT NULL,
   major ENUM('CS', 'Economics', 'Mathematics'),
   sequence ENUM('None', 'CS', 'Financial-Economics', 'Leadership')
 );
@@ -17,19 +18,19 @@ CREATE TABLE Students (
 CREATE TABLE Major (
   m_id INT UNSIGNED NOT NULL PRIMARY KEY,
   credits FLOAT(3,2) NOT NULL,
-  college ENUM('CMC','HMC','Scripps') NOT NULL
+  m_col ENUM('Claremont McKenna College','Harvey Mudd College','Scripps College') NOT NULL
 );
 
 CREATE TABLE Sequence (
   q_id INT UNSIGNED NOT NULL PRIMARY KEY,
-  credits FLOAT(3,2) NOT NULL
-  college ENUM('CMC','HMC','Scripps') NOT NULL
+  credits FLOAT(3,2) NOT NULL,
+  q_col ENUM('Claremont McKenna College','Harvey Mudd College','Scripps College') NOT NULL
 );
 
 CREATE TABLE Courses (
   c_id INT UNSIGNED NOT NULL PRIMARY KEY,
   credits FLOAT(3,2) NOT NULL,
-  college ENUM('CMC', 'HMC','Scripps') NOT NULL
+  c_col ENUM('Claremont McKenna College','Harvey Mudd College','Scripps College') NOT NULL
 );
 
 CREATE TABLE Completed (
