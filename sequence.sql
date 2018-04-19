7b71d226915b73beac471b805d617cf257cc334d
Annalise Ko <annaliseliuko@gmail.com>
2018-04-19 13:54:57 -0700

    insert data

--- sequence.sql
+++ sequence.sql
@@ -11,26 +11,39 @@ CREATE TABLE Students (
   pwd VARCHAR(256) NOT NULL,
   college ENUM('Claremont McKenna College','Harvey Mudd College','Scripps College') NOT NULL,
   grad INT(4) UNSIGNED NOT NULL,
-  major ENUM('CS', 'Economics', 'Mathematics'),
-  sequence ENUM('None', 'CS', 'Financial-Economics', 'Leadership')
+  major ENUM('Computer Science', 'Economics', 'Mathematics'),
+  sequence ENUM('None', 'Computer Science', 'Financial Economics', 'Leadership')
 );
 
 CREATE TABLE Major (
   m_id INT UNSIGNED NOT NULL PRIMARY KEY,
-  credits FLOAT(3,2) NOT NULL,
-  m_col ENUM('Claremont McKenna College','Harvey Mudd College','Scripps College') NOT NULL
+  m_name ENUM('Computer Science', 'Economics', 'Mathematics'),
+  credits FLOAT(4,2) NOT NULL
 );
+
+INSERT INTO Major (m_id, m_name, credits)
+VALUES
+   ('0', 'Computer Science', '13.00'),
+   ('1', 'Economics', '11.00'),
+   ('2', 'Mathematics', '12.00');
+
 
 CREATE TABLE Sequence (
   q_id INT UNSIGNED NOT NULL PRIMARY KEY,
-  credits FLOAT(3,2) NOT NULL,
-  q_col ENUM('Claremont McKenna College','Harvey Mudd College','Scripps College') NOT NULL
+  q_name ENUM('None', 'Computer Science', 'Financial Economics', 'Leadership'),
+  credits FLOAT(3,2) NOT NULL
 );
+
+INSERT INTO Sequence (q_id, q_name, credits)
+VALUES
+    ('0', 'None', '0.00'),
+    ('1', 'Computer Science', '6.00'),
+    ('2', 'Financial Economics', '5.00'),
+    ('3', 'Leadership', '5.00');
 
 CREATE TABLE Courses (
   c_id INT UNSIGNED NOT NULL PRIMARY KEY,
-  credits FLOAT(3,2) NOT NULL,
-  c_col ENUM('Claremont McKenna College','Harvey Mudd College','Scripps College') NOT NULL
+  credits FLOAT(4,2) NOT NULL
 );
 
 CREATE TABLE Completed (
